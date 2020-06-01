a94ceb5af412d224f42b685e9c5b79262919acf9
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-06-01 13:46:13 -0700

    changed some hyper parameters

--- main.py
+++ main.py
@@ -13,118 +13,119 @@ import csv
 #---------------------#
 # Signal Settings
 preloaded_signal = False
-save_signal      = True
-peaks_to_excel   = True
-use_intervals    = True
+save_signal      = False
+peaks_to_excel   = False
+use_intervals    = False
 
 # Display Settings
-show_signal      = False
-show_freq_domain = False
-show_bandpass    = False
-show_derivatives = False
+show_signal      = True
+show_freq_domain = True
+show_bandpass    = True
+show_derivatives = True
 show_peaks       = True
 
-# Cycle through all file names
-for folder_name in files:
-     for dosage in files[folder_name]:
-          for interval_number in files[folder_name][dosage]["intervals"]:
+folder_name = "1 9 2020 AH TDMS ESSENTIAL"
+dosage = 10
+interval_number = 1
 
-               # Pick file
-               file_name = files[folder_name][dosage]["file_name"]
-               save_file_name = folder_name + "_" + file_name + "_d" + str(dosage) if use_intervals == False else folder_name + "_" + file_name + "_d" + str(dosage) + "_i" + str(interval_number)
+# Pick file
+file_name = files[folder_name][dosage]["file_name"]
+save_file_name = folder_name + "_" + file_name + "_d" + str(dosage) if use_intervals == False else folder_name + "_" + file_name + "_d" + str(dosage) + "_i" + str(interval_number)
 
-               # Pick Interval
-               if use_intervals == True and files[folder_name][dosage]["intervals"] != ["None"]:
-                    start_time = files[folder_name][dosage]["intervals"][interval_number][0]
-                    end_time   = files[folder_name][dosage]["intervals"][interval_number][1]
-               #---------------------------------------------------------------------------------#
-               # End of Hyperparameters
+# Pick Interval
+if use_intervals == True and files[folder_name][dosage]["intervals"] != ["None"]:
+     start_time = files[folder_name][dosage]["intervals"][interval_number][0]
+     end_time   = files[folder_name][dosage]["intervals"][interval_number][1]
+#---------------------------------------------------------------------------------#
+# End of Hyperparameters
 
-               # Initalize patient and interval
-               if preloaded_signal == False:
-                    # Change directory
-                    wd = 'data/' + folder_name + '/files_of_interest'
+# Initalize patient and interval
+if preloaded_signal == False:
+     # Change directory
+     wd = 'data/' + folder_name + '/files_of_interest'
 
-                    # Load TDMS file into Patient object
-                    patient = Patient(wd, file_name)
+     # Load TDMS file into Patient object
+     patient = Patient(wd, file_name)
 
-                    # Declare time and signal
-                    if use_intervals == True and files[folder_name][dosage]["intervals"] != ["None"]:
-                         start = (start_time - np.min(patient.times))*patient.frequency
-                         end   = patient.total_time*patient.frequency if end_time == "end" else (end_time - np.min(patient.times))*patient.frequency
-                         interval = range(int(start), int(end))
-                         time, signal = patient.get_interval(interval)
-                    else:
-                         time = patient.times
-                         signal = patient.ecg
-                    
-                    # Save signal
-                    if save_signal == True:
-                         np.savetxt('time_'  + save_file_name + '.csv', time, delimiter=',')
-                         np.savetxt('signal_'+ save_file_name + '.csv', signal, delimiter=',')
+     # Declare time and signal
+     if use_intervals == True and files[folder_name][dosage]["intervals"] != ["None"]:
+          start = (start_time - np.min(patient.times))*patient.frequency
+          end   = patient.total_time*patient.frequency if end_time == "end" else (end_time - np.min(patient.times))*patient.frequency
+          interval = range(int(start), int(end))
+          time, signal = patient.get_interval(interval)
+     else:
+          time = patient.times
+          signal = patient.ecg
+     
+     # Save signal
+     if save_signal == True:
+          np.savetxt('time_'  + save_file_name + '.csv', time, delimiter=',')
+          np.savetxt('signal_'+ save_file_name + '.csv', signal, delimiter=',')
 
-               else:
-                    os.chdir("../data/Derived")
-                    time   = np.loadtxt('time_' + save_file_name + '.csv', delimiter=',')
-                    signal = np.loadtxt('signal_' + save_file_name + '.csv', delimiter=',')
+else:
+     os.chdir("data/Derived")
+     time   = np.loadtxt('time_' + save_file_name + '.csv', delimiter=',')
+     signal = np.loadtxt('signal_' + save_file_name + '.csv', delimiter=',')
 
 
-               # Initalize heartbreaker
-               hb = HeartBreak()
+# Initalize heartbreaker
+hb = HeartBreak()
 
-               # View Signal
-               if show_signal == True:
-                    hb.plot_signal(time, signal)
+# View Signal
+if show_signal == True:
+     hb.plot_signal(time, signal)
 
-               # View Frequency Domain
-               if show_freq_domain == True:
-                    hb.get_fft(time = time, 
+# View Frequency Domain
+if show_freq_domain == True:
+     hb.get_fft(time = time, 
+                    signal = signal,
+                    plot = True)
+
+     hb.get_spectrum(time = time, 
+                         signal = signal)
+
+# Bandblock
+if show_bandpass == True:
+     bandpass_signal = hb.lowpass_filter(time = time, 
                                    signal = signal,
-                                   plot = True)
+                                   cutoff_freq = 10)
 
-                    hb.get_spectrum(time = time, 
-                                        signal = signal)
+     hb.get_fft(time = time, 
+                    signal = bandpass_signal,
+                    plot = True)
 
-               # Bandblock
-               if show_bandpass == True:
-                    bandpass_signal = hb.lowpass_filter(time = time, 
-                                                  signal = signal,
-                                                  cutoff_freq = 10)
+# View Derivatives
+if show_derivatives == True:
+     bandpass_signal = hb.bandpass_filter(time = time, 
+                                        signal = signal,
+                                        freqmin = 59, 
+                                        freqmax = 61)
+     hb.get_derivatives(signal = bandpass_signal, 
+                         plot =True)
 
-                    hb.get_fft(time = time, 
-                                   signal = bandpass_signal,
-                                   plot = True)
+# Find Peaks
+if show_peaks == True:
+     lowpass_signal = hb.bandpass_filter(time    = time, 
+                                        signal  = signal,
+                                        freqmin = 59, 
+                                        freqmax = 61)
 
-               # View Derivatives
-               if show_derivatives == True:
-                    bandpass_signal = hb.bandpass_filter(time = time, 
-                                                       signal = signal,
-                                                       freqmin = 59, 
-                                                       freqmax = 61)
-                    hb.get_derivatives(signal = bandpass_signal, 
-                                        plot =True)
+     # Low-Pass filter under 10Hz
+     lowpass_signal = hb.lowpass_filter(time = time, 
+                                   signal = lowpass_signal,
+                                   cutoff_freq = 50)
 
-               # Find Peaks
-               if show_peaks == True:
-                    lowpass_signal = hb.bandpass_filter(time    = time, 
-                                                       signal  = signal,
-                                                       freqmin = 59, 
-                                                       freqmax = 61)
+     peaks = hb.get_ecg_peaks(time = time, 
+                              signal = lowpass_signal,
+                              plot = True,
+                              plot_st_segments = True,
+                              plot_segmentation_decisons=False)
 
-                    # Low-Pass filter under 10Hz
-                    lowpass_signal = hb.lowpass_filter(time = time, 
-                                                  signal = lowpass_signal,
-                                                  cutoff_freq = 50)
-
-                    peaks = hb.get_ecg_peaks(time = time, 
-                                             signal = lowpass_signal,
-                                             plot = False)
-
-                    if peaks_to_excel == True:
-                         hb.save_peaks_to_excel(save_file_name, time, peaks)
-                    os.chdir("../../")
-                    
+     if peaks_to_excel == True:
+          hb.save_peaks_to_excel(save_file_name, time, peaks)
+     
+     
 
 
-                              
+               
 
